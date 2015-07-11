34a72c827ecde93891a4b4a470b849cf76160405
Ake Hedman, Paradise of the Frog <akhe@grodansparadis.com>
2015-07-11 17:33:59 +0200

    Added hex files

--- Bejing.X/nbproject/Makefile-impl.mk
+++ Bejing.X/nbproject/Makefile-impl.mk
@@ -27,7 +27,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 PROJECTNAME=Bejing.X
 
 # Active Configuration
-DEFAULTCONF=bejing_2580_debug
+DEFAULTCONF=bejing_2580_release
 CONF=${DEFAULTCONF}
 
 # All Configurations
