f6419ecef2cf57d398cd08026e5ee1f35cd0ea41
Ake Hedman <akhe@grodansparadis.com>
2016-03-22 16:14:36 +0100

    Firmware with new VSCP framworkd code

--- main.c
+++ main.c
@@ -22,12 +22,10 @@
  * ******************************************************************************
  */
 
-
-
 #include "vscp_compiler.h"
 #include "vscp_projdefs.h"
 
-#include <p18cxxx.h>
+#include <xc.h>
 #include <timers.h>
 #include <delays.h>
 #include <inttypes.h>
