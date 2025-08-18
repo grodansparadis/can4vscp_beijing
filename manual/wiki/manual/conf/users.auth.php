0fb95119654f3cadd860417d1e5ea8bb9d07b828
Ake Hedman <akhe@grodansparadis.com>
2025-08-18 14:30:06 +0200

    Convert schema to kicad

---
+++ manual/wiki/manual/conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+admin:$1$BT1AIyaS$X3sJZou6QZx4b1r72DZfO.:Ake Hedman:akhe@grodansparadis.com:admin,user
